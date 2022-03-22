7d5547664491acace4d89c8991b2aed0b4d6536a
Neruxov <k.neruxov@gmail.com>
2022-03-22 19:24:37 +0300

    Split classes into different files

---
+++ engine.py
@@ -0,0 +1,22 @@
+import requests
+
+from methods.player import *
+
+class API:
+    def __init__(self, apikey):
+        self.apikey = apikey
+
+    def player(self, name='', uuid=''):
+        '''
+        Gets player object
+        :param name: Name of the player (use one of these)
+        :param uuid: Uuid of the player (use one of these)
+        :return: player object
+        '''
+        if name:
+            data = requests.get(f"https://api.hypixel.net/player?name={name}&key={self.apikey}").json()
+        elif uuid:
+            data = requests.get(f"https://api.hypixel.net/player?uuid={uuid}&key={self.apikey}").json()
+        else:
+            return
+        return Player(data)
---
+++ methods/player.py
@@ -0,0 +1,3 @@
+class Player:
+    def __init__(self, data):
+        self.data = data
