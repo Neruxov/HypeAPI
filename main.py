e74c320d243abbb3e253f3b7a61f1801e0bac50f
Neruxov <k.neruxov@gmail.com>
2022-03-22 19:03:09 +0300

    Added API and Player class

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# darkman pedik
+import requests
+
+class API:
+    def __init__(self, apikey):
+        self.apikey = apikey
+
+    def player(self, name):
+        data = requests.get(f"https://api.hypixel.net/player?name={name}&key={self.apikey}")
+        return Player()
+
+class Player:
+    def __init__(self, data):
+        self.data = data
