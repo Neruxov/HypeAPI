d4e12354742563b55475b1102bb0ea580a2de2f8
ildar <ildar6176@gmail.com>
2022-03-23 15:22:39 +0300

    guild player stats

--- src/hypeapi/methods/guildplayer.py
+++ src/hypeapi/methods/guildplayer.py
@@ -1,8 +1,27 @@
 import requests
 
+
 class GuildPlayer:
     def __init__(self, data):
         self.data = data
 
-    def getRank(self):
+        self.uuid = self.getUuid()
+        self.exp_history = self.getExpHistory()
+        self.joindate = self.getJoinedDate()
+        self.rank = self.getPlayerRank()
+        self.questParticipation = self.getQuestParticipation()
 
+    def getPlayerRank(self):
+        return self.data['rank']
+
+    def getJoinedDate(self):
+        return self.data['joined']
+
+    def getExpHistory(self):
+        return self.data['expHistory']
+
+    def getQuestParticipation(self):
+        return self.data['questParticipation']
+
+    def getUuid(self):
+        return self.data['uuid']
