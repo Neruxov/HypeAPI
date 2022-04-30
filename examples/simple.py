90e65fb2fe1f5a9b242be123b143dbe6ac4c6f49
Neruxov <k.neruxov@gmail.com>
2022-04-30 22:27:52 +0300

    Modified examples to follow PEP8 standarts

--- examples/simple.py
+++ examples/simple.py
@@ -1,5 +1,5 @@
-from hypeapi import API # import HypeAPI
+from hypeapi import API  # import HypeAPI
 
-api = API('your_api_key') # create an API object, used to send requests to the API
-player = api.getPlayer(name="Neruxov") # get a Player object
-print(player.displayname) # print the player's displayname
+api = API('your_api_key')  # create an API object, used to send requests to the API
+player = api.getPlayer(name="Neruxov")  # get a Player object
+print(player.displayname)  # print the player's displayname
